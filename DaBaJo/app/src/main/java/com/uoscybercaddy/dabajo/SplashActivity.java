a606ffec7cdefba007733320175188efe8b4d067
dmltls94@gmail.com <dmltls94@gmail.com>
2021-10-15 22:31:40 +0900

    스플래쉬화면 구현

---
+++ DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.uoscybercaddy.dabajo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final String Tag = "SplashActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(Tag,"Application Running...");
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
