95a41d223ac5c8ffa48c75b8a9c0c591d1fecc3b
dmltls94@gmail.com <dmltls94@gmail.com>
2021-10-17 04:29:35 +0900

    구글 로그인 구현

--- DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/LoginActivity.java
+++ DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/LoginActivity.java
@@ -1,32 +1,52 @@
 package com.uoscybercaddy.dabajo;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 100;
+    GoogleSignInClient mGoogleSignInClient;
+
     private FirebaseAuth mAuth;
+
+    SignInButton googleLoginBtn;
     EditText emailEditText, passwordEditText;
     ProgressDialog progressDialog;
     @Override
@@ -36,21 +56,38 @@ public class LoginActivity extends AppCompatActivity {
 
         emailEditText = (EditText) findViewById(R.id.emailEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
-
+        googleLoginBtn = (SignInButton) findViewById(R.id.googleLoginBtn);
         //Actionbar and its title
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("로그인");
         //enable back button
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
-
+        setGooglePlusButtonText(googleLoginBtn,"구글 로그인");
         // Initialize Firebase Auth
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //before mAuth
+
         mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.loginButton).setOnClickListener(onClickListener);
         findViewById(R.id.gotoSignupButton).setOnClickListener(onClickListener);
-
+        findViewById(R.id.findPassword).setOnClickListener(onClickListener);
+        googleLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+        //findViewById(R.id.gotoSignUpTV).setOnClickListener(onClickListener);
         progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("로그인 중...");
     }
     @Override
     public boolean onSupportNavigateUp() {
@@ -65,7 +102,20 @@ public class LoginActivity extends AppCompatActivity {
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(1);
     }
+    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
 
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText(buttonText);
+                tv.setPadding(0, 8, 0, 0);
+                return;
+            }
+        }
+
+    }
     @Override
     public void onStart() {
         super.onStart();
@@ -83,14 +133,128 @@ public class LoginActivity extends AppCompatActivity {
                 case R.id.gotoSignupButton:
                     startSignupActivity();
                     break;
+                case R.id.findPassword:
+                    showFindPasswordDialog();
+                    break;
+                //case R.id.gotoSignUpTV:
+                  //  startSignupActivity();
+                    //break;
                 case R.id.loginButton:
                     login();
                     break;
+
             }
         }
     };
 
+    private void showFindPasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("비밀번호 찾기");
 
+        LinearLayout linearLayout = new LinearLayout(this);
+        EditText emailEt = new EditText(this);
+        emailEt.setHint("이메일");
+        emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        emailEt.setMinEms(16);
+
+        linearLayout.addView(emailEt);
+        linearLayout.setPadding(10,10,10,10);
+        builder.setView(linearLayout);
+        builder.setPositiveButton("보내기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String email = emailEt.getText().toString().trim();
+                beginFind(email);
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        //show dialog
+        builder.create().show();
+    }
+
+    private void beginFind(String email) {
+        progressDialog.setMessage("이메일 전송중...");
+        progressDialog.show();
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                progressDialog.dismiss();
+                if(task.isSuccessful()){
+                    startToast("전송완료");
+                }else{
+                    startToast("전송실패");
+                    Log.w(TAG, "SendEmailResetMail:failure", task.getException());
+                    if (task.getException() != null) {
+                        System.out.println(task.getException().toString());
+                    }
+                    try {
+                        throw task.getException();
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                        startToast(""+e.getMessage());
+                    }
+                }
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                startToast(""+e.getMessage());
+                Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startToast("로그인 성공 : "+user.getEmail());
+                            startMainActivity();
+                            finish();
+                            //updateUI(user);
+                        } else {
+                            startToast("로그인 실패");
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //updateUI(null);
+                            if (task.getException() != null) {
+                                System.out.println(task.getException().toString());
+                            }
+                            try {
+                                throw task.getException();
+                            } catch (Exception e) {
+                                Log.e(TAG, e.getMessage());
+                                startToast(""+e.getMessage());
+                            }
+                        }
+                    }
+                });
+    }
     private void login() {
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
@@ -103,6 +267,7 @@ public class LoginActivity extends AppCompatActivity {
             passwordEditText.setFocusable(true);
         }
         else {
+            progressDialog.setMessage("로그인 중...");
             progressDialog.show();
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
