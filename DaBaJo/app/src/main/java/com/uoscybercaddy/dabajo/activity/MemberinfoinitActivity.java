75d40d008cde377dba818b2abbcabb7c33ce3ee3
dmltls94@gmail.com <dmltls94@gmail.com>
2021-10-30 21:14:00 +0900

    안쓰는 기능들 지움

--- DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/activity/MemberinfoinitActivity.java
+++ DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/activity/MemberinfoinitActivity.java
@@ -284,50 +284,6 @@ public class MemberinfoinitActivity extends AppCompatActivity {
         super.onBackPressed();
         finish();
     }
-    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
-
-        Matrix matrix = new Matrix();
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_NORMAL:
-                return bitmap;
-            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
-                matrix.setScale(-1, 1);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                matrix.setRotate(180);
-                break;
-            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-                matrix.setRotate(180);
-                matrix.postScale(-1, 1);
-                break;
-            case ExifInterface.ORIENTATION_TRANSPOSE:
-                matrix.setRotate(90);
-                matrix.postScale(-1, 1);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                matrix.setRotate(90);
-                break;
-            case ExifInterface.ORIENTATION_TRANSVERSE:
-                matrix.setRotate(-90);
-                matrix.postScale(-1, 1);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                matrix.setRotate(-90);
-                break;
-            default:
-                return bitmap;
-        }
-        try {
-            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-            bitmap.recycle();
-            return bmRotated;
-        }
-        catch (OutOfMemoryError e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -351,36 +307,7 @@ public class MemberinfoinitActivity extends AppCompatActivity {
         }
     }
 
-/*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode){
-            case 0:
-                if(resultCode == Activity.RESULT_OK){
-                    profilePath = data.getStringExtra("profilePath");
-                    Log.e("로그: ","profilePath: "+profilePath);
-                    ExifInterface exif = null;
-                    try {
-                        exif = new ExifInterface(profilePath);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                            ExifInterface.ORIENTATION_UNDEFINED);
 
-                    Bitmap bmp = BitmapFactory.decodeFile(profilePath);
-                    Bitmap bmRotated = rotateBitmap(bmp, orientation);
-
-
-
-                    profileImageView.setImageBitmap(bmRotated);
-
-                }
-                break;
-        }
-    }
-*/
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
