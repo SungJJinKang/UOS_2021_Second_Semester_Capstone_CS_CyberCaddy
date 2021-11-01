f584def6ea0a454dc8ecf51bb0ea8dc973064ff6
sy8044 <sy8044@naver.com>
2021-11-01 20:10:30 +0900

    피드 작업 커밋

--- DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/activity/FeedActivity.java
+++ DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/activity/FeedActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,17 +18,34 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.uoscybercaddy.dabajo.R;
+import com.uoscybercaddy.dabajo.adapter.PostAdapter;
+import com.uoscybercaddy.dabajo.view.WriteInfo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class FeedActivity extends AppCompatActivity  {
+
+    private RecyclerView feedRecyclerView;
+    private PostAdapter mAdapter;
+    private List<WriteInfo> mDatas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
         findViewById(R.id.writePostButton).setOnClickListener(onClickListener);
+        feedRecyclerView = findViewById(R.id.feedRecyclerView);
+        mDatas = new ArrayList<>();
+        mDatas.add(new WriteInfo("title","contents","writer",new Date()));
+        mDatas.add(new WriteInfo("title","contents","writer",new Date()));
+        mDatas.add(new WriteInfo("title","contents","writer",new Date()));
 
-
-    }
+        mAdapter = new PostAdapter(mDatas);
+        feedRecyclerView.setAdapter(mAdapter);
+        }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
