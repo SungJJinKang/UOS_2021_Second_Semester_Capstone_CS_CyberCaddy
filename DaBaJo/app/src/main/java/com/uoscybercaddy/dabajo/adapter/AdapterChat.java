9dd73a3a3295c90761cbf0fb7e09b16dd5fb0dfc
dmltls94@gmail.com <dmltls94@gmail.com>
2021-11-06 00:01:05 +0900

    채팅 메세지 삭제 구현

--- DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/adapter/AdapterChat.java
+++ DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/adapter/AdapterChat.java
@@ -1,5 +1,6 @@
 package com.uoscybercaddy.dabajo.adapter;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,17 +12,30 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.uoscybercaddy.dabajo.R;
 import com.uoscybercaddy.dabajo.activity.ChatActivity;
 import com.uoscybercaddy.dabajo.models.Modelchat;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -34,7 +48,7 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
     List<Modelchat> chatList;
     String imageUrl;
     FirebaseUser fUser;
-
+    private static final String TAG = "AdapterChat";
     public AdapterChat(Context context, List<Modelchat> chatList, String imageUrl) {
         this.context = context;
         this.chatList = chatList;
@@ -54,7 +68,7 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyHolder holder, @SuppressLint("RecyclerView") int position) {
         String message = chatList.get(position).getMessage();
         String timeStamp = chatList.get(position).getTimeStamp();
         Calendar cal = Calendar.getInstance(Locale.KOREA);
@@ -70,27 +84,28 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
         }catch (Exception e){
             Glide.with(context).load(R.drawable.ic_profile_black).centerCrop().override(500).into(holder.profileIv);
         }
-        holder.messageLayout.setOnClickListener(new View.OnClickListener() {
+        holder.messageLayout.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onLongClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setTitle("메세지 삭제");
                 builder.setMessage("정말 삭제하시겠습니까?");
                 builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        deleteMessage(position);
                     }
                 });
                 builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        dialog.dismiss();
                     }
                 });
+                builder.create().show();
+                return false;
             }
         });
-
         if(position == chatList.size()-1){
             if(chatList.get(position).isSeen()){
                 holder.isSeenTv.setText("읽음");
@@ -101,6 +116,49 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
             holder.isSeenTv.setVisibility(View.GONE);
         }
 
+    }
+
+    private void deleteMessage(int position) {
+        String myUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String msgTimeStamp = chatList.get(position).getTimeStamp();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference citiesRef = db.collection("chats");
+
+        citiesRef.whereEqualTo("timestamp",msgTimeStamp)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+                        for (QueryDocumentSnapshot doc : value) {
+                            if (doc.get("sender") != null) {
+                                if(doc.getString("sender").equals(myUid)){
+                                    doc.getReference().update("message", "메세지가 삭제됐습니다...")
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d(TAG, "메세지삭제 ");
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w(TAG, "Error writing document", e);
+                                                }
+                                            });
+                                    Toast.makeText(context,"메세지가 삭제됐습니다...",Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    Toast.makeText(context,"자기가 보낸 메세지만 삭제가 가능합니다.",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                        Log.d(TAG, "Current cites in CA: " );
+                    }
+                });
     }
 
     @Override
