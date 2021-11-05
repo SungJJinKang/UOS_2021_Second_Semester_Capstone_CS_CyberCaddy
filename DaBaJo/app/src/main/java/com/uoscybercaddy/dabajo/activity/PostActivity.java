4e4f9db750022d5162ea68868b60d1b8c112a1ae
sy8044 <sy8044@naver.com>
2021-11-05 22:12:03 +0900

    test

--- DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/activity/PostActivity.java
+++ DaBaJo/app/src/main/java/com/uoscybercaddy/dabajo/activity/PostActivity.java
@@ -2,19 +2,28 @@ package com.uoscybercaddy.dabajo.activity;
 
 import static com.uoscybercaddy.dabajo.activity.FeedVideoImgHelper.SetImageToImageView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.uoscybercaddy.dabajo.R;
 import com.uoscybercaddy.dabajo.view.WriteInfo;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class PostActivity extends AppCompatActivity {
@@ -45,12 +54,32 @@ public class PostActivity extends AppCompatActivity {
         Glide.with(this).load(img_url).override(1000).into(imageView);
         parent.addView(imageView);
         final int imgCount = writeInfo.imageCount;
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        ArrayList<String> imgDirectoryList = ImageDirectoryHelper.GetImageDirecotry(writeInfo);
+
         if(imgCount>0) {
             for(int i=0; i<imgCount; i++) {
                 ImageView imageView2 = new ImageView(PostActivity.this);
-                imageView.setLayoutParams(layoutParams);
-                SetImageToImageView(imageView, writeInfo, i);
-                parent.addView(imageView);
+                imageView2.setLayoutParams(layoutParams);
+                String temp = imgDirectoryList.get(i);
+                storageRef.child(temp).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Got the download URL for 'users/me/profile.png'
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                    }
+                });
+                Glide.with(this).load(temp).override(1000).into(imageView2);
+//                Bitmap bitmap = GetImageFromWriteInfo(writeInfo, i);
+//                imageView.setImageBitmap(bitmap);
+                parent.addView(imageView2);
 
             }
         }
